b4e1709746c240076e1d74a783e1507aa22a9337
Kanahiro <mediterranean1769@gmail.com>
2025-08-14 23:37:12 +0900

    cdk

---
+++ cdk/lib/cdk-stack.ts
@@ -0,0 +1,23 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+export class CdkStack extends cdk.Stack {
+	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+		super(scope, id, props);
+
+		const nodejsFunction = new cdk.aws_lambda_nodejs.NodejsFunction(
+			this,
+			'NodejsFunction',
+			{
+				runtime: cdk.aws_lambda.Runtime.NODEJS_22_X,
+				entry: 'lambda/index.ts',
+				handler: 'handler',
+				architecture: cdk.aws_lambda.Architecture.ARM_64,
+			},
+		);
+
+		nodejsFunction.addFunctionUrl({
+			authType: cdk.aws_lambda.FunctionUrlAuthType.NONE,
+		});
+	}
+}
